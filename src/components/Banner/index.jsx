3337b2c5e9e23f640202d049ddba127216235012
Ale Raf <raffinalexis@gmail.com>
2023-03-19 17:25:39 +0100

    Banner path condition

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,20 +1,13 @@
-import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../../styles/Banner.css'
 
 function Banner() {
     let location = useLocation()
     let path = location.pathname
-    const [update, setUpdate] = useState(true)
-
-    useEffect(() => {
-            if (path === '/about')
-            setUpdate(!update)
-    }, [])
 
     return (
-        <div className={ update ? 'home-banner' : 'about-banner' }>
-            { update ? <h1 className="home-title">Chez vous, partout et ailleurs</h1> : null }
+        <div className={ path === '/' ? 'home-banner' : 'about-banner' }>
+            { path === '/' ? <h1 className="home-title">Chez vous, partout et ailleurs</h1> : null }
         </div>
     )
 }
